3b23a9ad9dabdd2560e797b3936769d389d920be
Lucas Ribeiro <lucasribeiro5569@gmail.com>
2021-06-25 22:14:06 -0300

    remove WAIncognito and create new solution

--- zapzappy/__init__.py
+++ zapzappy/__init__.py
@@ -14,7 +14,7 @@ class Whatsapp_api:
         options.add_argument("--user-data-dir=data_chrome")
         options.add_argument("--hide-scrollbars")
         options.add_argument("--disable-gpu")
-        options.add_extension('./extensions/extension_1_7_0_0.crx')
+        #options.add_extension('extensions/extension_1_7_0_0.crx')
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36')
         self.driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
@@ -59,6 +59,8 @@ class Whatsapp_api:
                 else:
                     chat.send_keys(message)
                     chat.send_keys(Keys.ENTER)
+                user = self.driver.find_element_by_xpath(f"//span[@title='i']")
+                user.click()
                 return True          
         except:
             return False
